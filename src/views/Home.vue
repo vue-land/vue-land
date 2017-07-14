3450a9984fd2ee4495756412e3ee179d26e8f377
EGOIST <0x142857@gmail.com>
2017-07-14 23:07:15 +0800

    use new invitation link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
           Can you imagine the whole Vue community fit in a single Discord server? Come chat about Vue, JavaScript stuffs or ask for help!
         </p>
       </div>
-      <a class="button button-shadow" href="https://discord.gg/eKWPvZ9">Join us on Discord</a>
+      <a class="button button-shadow" href="https://discordapp.com/invite/HBherRA">Join us on Discord</a>
     </div>
     <site-footer />
   </div>
