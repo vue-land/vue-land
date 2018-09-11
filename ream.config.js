523b5e61f5e1016f5fbe1bf0f972a81311a8d3f9
gustojs <gustojs@protonmail.com>
2018-09-11 20:54:11 +0200

    Remove "/" from "/q-and-a/"

--- ream.config.js
+++ ream.config.js
@@ -6,7 +6,7 @@ module.exports = {
       "/for-library",
       "/polls",
       "/guideline",
-      "/q-and-a/",
+      "/q-and-a",
       "/q-and-a/01-guillaume-chau-evan-you",
       "/q-and-a/02-damian-dulisz-chris-fritz"
     ]
