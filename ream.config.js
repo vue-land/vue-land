d8e169e575eadcee2410c78e46538231b6129fbd
gustojs <gustojs@protonmail.com>
2018-09-11 20:46:16 +0200

    Add new routes to generate

--- ream.config.js
+++ ream.config.js
@@ -1,15 +1,24 @@
 module.exports = {
-  entry: 'src/index.js',
+  entry: "src/index.js",
   generate: {
-    routes: ['/', '/for-library', '/polls', '/guideline']
+    routes: [
+      "/",
+      "/for-library",
+      "/polls",
+      "/guideline",
+      "/q-and-a/",
+      "/q-and-a/01-guillaume-chau-evan-you",
+      "/q-and-a/02-damian-dulisz-chris-fritz"
+    ]
   },
   chainWebpack(config) {
-    config.module.rule('md')
+    config.module
+      .rule("md")
       .test(/\.md$/)
-      .use('vue-loader')
-        .loader('vue-loader')
-        .end()
-      .use('vmark-loader')
-        .loader('vmark-loader')
+      .use("vue-loader")
+      .loader("vue-loader")
+      .end()
+      .use("vmark-loader")
+      .loader("vmark-loader");
   }
-}
+};
