f4310adc7414a1e052758824923f737993018f51
gustojs <gustojs@protonmail.com>
2018-09-11 22:53:54 +0200

    Add Q&A description and link to Twitter

--- src/views/QandA.vue
+++ src/views/QandA.vue
@@ -3,9 +3,19 @@
     <site-header />
     <div class="site-main">
       <div class="site-content">
-        <h3>Question & Answers sessions on VueLand</h3>
-        <p><router-link to="/01-guillaume-chau-evan-you">#01 - Vue CLI 3 with Guillaume Chau and Evan You</router-link></p>
-        <p><router-link to="/02-damian-dulisz-chris-fritz">#02 - Best practices with Damian Dulisz and Chris Fritz</router-link></p>
+        <h3>Q&amp;A sessions on VueLand</h3>
+        <p>Q&amp;A sessions are the best opportunity to ask your questions directly to the stars of Vue.js ecosystem. We're going to organize them once or twice a month, on Saturdays, usually at 4pm UTC. Each event will have a specific topic and there will be two experts to answer the questions on the matter.</p>
+        <p>The events are announced both on the <channel name="announcements" /> channel and on our official <a href="https://twitter.com/VueLandDiscord">Twitter account</a>.</p>
+
+        <p>You can find the transcriptions from our previous events below:</p>
+        <p>
+          #01 - <router-link to="/01-guillaume-chau-evan-you">Vue CLI 3 with Guillaume Chau and Evan You</router-link>
+          <small>August the 18th, 2018</small>
+        </p>
+        <p>
+          #02 - <router-link to="/02-damian-dulisz-chris-fritz">Best practices with Damian Dulisz and Chris Fritz</router-link>
+          <small>September the 8th, 2018</small>
+        </p>
       </div>
     </div>
     <site-footer />
@@ -15,6 +25,7 @@
 <script>
 import SiteHeader from "../components/Header.vue";
 import SiteFooter from "../components/Footer.vue";
+import Channel from "../components/Channel.vue";
 
 export default {
   name: "q-and-a",
@@ -23,7 +34,14 @@ export default {
   },
   components: {
     SiteHeader,
-    SiteFooter
+    SiteFooter,
+    Channel
   }
 };
 </script>
+
+<style lang="stylus" scoped>
+small {
+  display: block;
+}
+</style>
