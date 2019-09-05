ded50462a59f08615630ad8545cb14cbe91f987c
EGOIST <0x142857@gmail.com>
2019-09-05 20:11:15 +0800

    properly redirect

--- src/saber-browser.js
+++ src/saber-browser.js
@@ -4,6 +4,12 @@ import 'typeface-work-sans/index.css'
 import './css/shared.styl'
 import './css/reset.styl'
 
+if (process.browser) {
+  if (location.hostname === 'vue-land.js.org') {
+    location.href = `https://vue.land${location.pathname}`
+  }
+}
+
 export default ({ setRootComponent }) => {
   setRootComponent(App)
 }
